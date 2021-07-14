f93f42affa4ceb803af3744e085ce28d83d0d34a
mapei <9725029@qq.com>
2021-07-14 23:00:10 +0800

    eureka client 完成

--- project-provider-user-8001/src/main/java/com/mapei/www/UserProvider8001_App.java
+++ project-provider-user-8001/src/main/java/com/mapei/www/UserProvider8001_App.java
@@ -4,9 +4,11 @@ package com.mapei.www;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan({"com.mapei.www.dao"})
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class UserProvider8001_App {
 
     public static void main(String[] args) {
--- project-web-80/src/main/java/com/mapei/www/ProjectWeb80_App.java
+++ project-web-80/src/main/java/com/mapei/www/ProjectWeb80_App.java
@@ -3,8 +3,10 @@ package com.mapei.www;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class ProjectWeb80_App {
 
     public static void main(String[] args)
