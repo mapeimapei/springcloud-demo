6888b33707e39adeb00034d520f8362bda422fe4
mapei <9725029@qq.com>
2021-07-19 21:55:57 +0800

    jwt + shiro 权限控制完成

--- project-web-80/src/main/java/com/mapei/www/controller/UserController.java
+++ project-web-80/src/main/java/com/mapei/www/controller/UserController.java
@@ -4,6 +4,8 @@ import com.mapei.www.result.ExceptionMsg;
 import com.mapei.www.result.ResponseData;
 import com.mapei.www.service.TbUserService;
 import com.mapei.www.util.Assgin;
+import com.mapei.www.util.JWTUtil;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,41 +25,28 @@ public class UserController {
 
     @PostMapping("/api2/login")
     public ResponseData login(@RequestBody Map params) throws IllegalAccessException {
-
         String email = (String) params.get("account");
         String password = (String) params.get("passwd");
-
         System.out.println(email + password);
-
         if (password.isEmpty() || null == password) {
             throw new ValidationException("密码不能为空");
         }
-
         String passwd = new Md5Hash(password, "mapei", 2).toString();
         Map user = (Map) tbUserService.getUser(email);
-
-        user.put("token","aaaaaaaaaaaaaaa121212432534");
-
-        System.out.println(user);
-
-
-        return new ResponseData(ExceptionMsg.SUCCESS, user);
-
+        if (user.get("passwd").equals(passwd)) {
+            user.put("token", JWTUtil.sign(email, passwd));
+            String[] keys = {"user_id", "name", "email", "token", "admin"};
+            return new ResponseData(ExceptionMsg.SUCCESS, user, keys);
+        } else {
+            return new ResponseData(ExceptionMsg.UNAUTHORIZED);
+        }
     }
 
-
+    @RequiresAuthentication
     @RequestMapping(value = "/api2/user/getUser")
-    public ResponseData getUser(@RequestParam(value="username", required=true) String username) throws Exception
-    {
+    public ResponseData getUser(@RequestParam(value = "username", required = true) String username) throws Exception {
         Object user = tbUserService.getUser(username);
-
-//        Map<String, Object> addProperties = new HashMap();
-//        addProperties.put("token","aaaaaaaaaaaaaaa121212432534");
-//        Object loginInf = Assgin.main(user, addProperties);
-
-
         return new ResponseData(ExceptionMsg.SUCCESS, user);
-        //return restTemplate.getForObject(REST_URL_PREFIX + "/user/get/" + id, TbUser.class);
     }
 
 }
--- project-web-80/src/main/java/com/mapei/www/exception/ValidatorUtils.java
+++ project-web-80/src/main/java/com/mapei/www/exception/ValidatorUtils.java
@@ -17,7 +17,7 @@ public class ValidatorUtils {
      * @param object 待校验对象
      * @param groups 待校验的组
      * @throws ValidationException 校验不通过，则报ValidationException异常
-
+     */
     public static void validateEntity(Object object, Class<?>... groups) {
         Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
         if (!constraintViolations.isEmpty()) {
@@ -29,5 +29,5 @@ public class ValidatorUtils {
             });
         }
     }
-     */
+
 }
